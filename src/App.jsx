ecec5ffb6b657e1629fa705bd10d259896ca02c2
Ridwan <ridwanyinusa15@gmail.com>
2024-09-08 09:26:26 +0100

    chore: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+import Navbar from './components/Navbar.jsx';
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar />
+    </main>
+  );
+};
 
 export default App;
