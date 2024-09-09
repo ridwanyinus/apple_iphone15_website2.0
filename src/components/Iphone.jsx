dd598e93cc73c296fe61cf5bf282d14af05e437a
Ridwan <ridwanyinusa15@gmail.com>
2024-09-09 09:19:57 +0100

    chore: fix bug

--- src/components/Iphone.jsx
+++ src/components/Iphone.jsx
@@ -6,14 +6,14 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 import { useEffect } from 'react';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { useGLTF, useTexture } from '@react-three/drei';
 import * as THREE from 'three';
 function Model(props) {
   const { nodes, materials } = useGLTF('/models/scene.glb');
 
   const texture = useTexture(props.item.img);
-  
+
   useEffect(() => {
     Object.entries(materials).map((material) => {
       // these are the material names that can't be changed color
