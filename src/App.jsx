8be9437df948c282095591be4a5afb07301a7eb6
Ridwan <ridwanyinusa15@gmail.com>
2024-09-10 17:06:41 +0100

    chore: configure sentry

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Highlights from './components/Highlights.jsx';
 import Model from './components/Model.jsx';
+import * as Sentry from '@sentry/react';
+
 const App = () => {
   return (
-    <main className='bg-black'>
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-    </main>
+    <button className='text-white' onClick={() => methodDoesNotExist()}>
+      Break the world
+    </button>
   );
+
+  // return (
+  //   <main className='bg-black'>
+  //     <Navbar />
+  //     <Hero />
+  //     <Highlights />
+  //     <Model />
+  //   </main>
+  // );
 };
 
-export default App;
+export default Sentry.withProfiler(App); // App;
