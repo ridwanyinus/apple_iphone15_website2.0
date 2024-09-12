dd13e87f60fc6ee9a50de39be7bbb392b21a344d
Ridwan <ridwanyinusa15@gmail.com>
2024-09-12 11:52:05 +0100

    chore: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from './components/Model.jsx';
 import * as Sentry from '@sentry/react';
 import Features from './components/Features.jsx';
 import HowItWorks from './components/HowItWorks.jsx';
+import Footer from './components/Footer.jsx';
 const App = () => {
   return (
     <main className='bg-black'>
@@ -14,6 +15,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
