4b5a19e258ad4c8dd43a43f2dd69156482911296
Ridwan <ridwanyinusa15@gmail.com>
2024-12-01 12:21:23 +0100

    add dynamic year to copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { footerLinks } from '../constants/index';
+const date = new Date();
 const Footer = () => {
   return (
     <footer className='py-5 sm:px-10 px-5'>
@@ -18,7 +19,7 @@ const Footer = () => {
         <div className='bg-neutral-700 my-5 h-[1px] w-full'></div>
 
         <div className='flex md:flex-row flex-col md:items-center justify-between'>
-          <p className='text-xs text-gray font-semibold'>Copyright @ 2024 Apple Inc. All rights reserved. </p>
+          <p className='text-xs text-gray font-semibold'>Copyright @ {date.getFullYear()} Apple Inc. All rights reserved. </p>
 
           <div className='flex flex-wrap'>
             {footerLinks.map((link, i) => (
